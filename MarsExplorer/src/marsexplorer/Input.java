4a2d9a7b74a90360fdb0f650b80f04d120cfcae0
James Goodricke <jggoodricke@gmail.com>
2019-03-15 16:52:50 +1100

    Added input functionality
    -All methods implemented and working.
    -Currently passes all unit tests (waiting on additional tests)

--- MarsExplorer/src/marsexplorer/Input.java
+++ MarsExplorer/src/marsexplorer/Input.java
@@ -16,14 +16,72 @@ package marsexplorer;
 * @since   2019-03-14 
 */
 public abstract class Input {
+    private Rover r = new Rover();
+    
+    //Method for interpreting input Commands and sending them to Rover
     protected String executeCommand(String input){
-        //TODO: Finish This
-        return null;
+
+        //Strip spaces and change case of input
+        input =  input.trim();
+        input = input.toUpperCase();
+        
+        //Check report first, as it has no arguments
+        if(input.equals("REPORT")){
+            return r.report();
+        }
+        
+        //If not report, begin processing input to get command and arguments
+        else{
+            moveAndPlace(input);
+            //If not reporting, return empty string
+            return ""; 
+        }
+
     }
     
-    private String[] format(String input){
-         //TODO: Finish This
-         return null;
+    private void moveAndPlace(String input) {
+        String command; //Holds the input command (MOVE, or PLACE)
+        String[] arguments; //Holds x and y position arguments
+        boolean argsCorrect = true; //Used in check if arguments are int
+        
+        int x = -1; //x position argument
+        int y = -1; //y position argument
+        
+        String[] inputSplit; //Temporarily holds input for procesing
+
+        //Split input into command and arguments and check it has two parts
+        inputSplit = input.split(" ");
+        if (inputSplit.length == 2) {
+            
+            //Extract command and arguments
+            command = inputSplit[0];
+            arguments = inputSplit[1].split(",");
+            argsCorrect = true;
+
+            //Check number of arguments and if they are integers
+            if (arguments.length == 2) {
+                try {
+                    x = Integer.parseInt(arguments[0]);
+                    y = Integer.parseInt(arguments[1]);
+                } catch (Exception e) {
+                    argsCorrect = false;
+                }
+            }
+            else{
+                argsCorrect = false;
+            }
+
+            //If arguments are integers, process command
+            if (argsCorrect) {
+                //Move
+                if (command.equals("MOVE")) {
+                    r.move(x, y);
+                } //Place
+                else if (command.equals("PLACE")) {
+                    r.place(x, y);
+                }
+            }
+        }
     }
     
     public abstract void runSim();
