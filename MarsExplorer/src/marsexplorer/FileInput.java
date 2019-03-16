8859b41ca6033a67afd30bc46ab74a8479b6c5d6
James Goodricke <jggoodricke@gmail.com>
2019-03-16 18:05:39 +1100

    File Input
    Added File input functionality and tested it to confirm it works as expected.

--- MarsExplorer/src/marsexplorer/FileInput.java
+++ MarsExplorer/src/marsexplorer/FileInput.java
@@ -7,6 +7,12 @@
 
 package marsexplorer;
 
+import java.util.Scanner;
+import java.io.BufferedReader; 
+import java.io.FileReader;
+import java.io.File;
+
+
 /**
 * Reads input commands from the command line to given to a rover object. 
 * Inherits most of its methods from the Input class.
@@ -18,5 +24,56 @@ package marsexplorer;
 public class FileInput extends Input {
     public void runSim(){
         
+        //Objects for reading user input and file input
+        Scanner sc = new Scanner(System.in);
+        BufferedReader br;
+        File file;
+        String filepath;
+        
+        //Stores input from file and output from rover
+        String input = ""; 
+        String output = "";
+        boolean moreFile = true;
+        
+        //Give instructions about where to place file before continuing.
+        System.out.println ("Please specify the path to your input file:");
+        filepath = sc.nextLine();
+        
+        //Strip double quotes from start and end of filepath
+        filepath = filepath.replaceAll("^\"|\"$", "");
+        
+        //Attempt to access file. If unable to access file, terminate program.
+        try{
+           file = new File(filepath);
+           br = new BufferedReader(new FileReader(file));
+        }
+        catch(Exception  e){
+            System.out.println("Error: File not found.");
+            return;
+        }      
+        
+        //Contunue reading file until no lines left
+        do {
+            //Attempt to read file. If unable to, report error and terminate program
+            try{
+                input = br.readLine();
+            }
+            catch(Exception E){
+                System.out.println("Error: Unable to read next line in file.");
+                break;
+            }
+            //Print input from file, execute input and print relevant output
+            if(input != null){
+                System.out.println(input);
+                output = executeCommand(input);
+                if(output != ""){
+                    System.out.println(output);
+                }
+            }
+        } while (input != null);
+        
+        //Pause program before terminating
+        System.out.println ("Press Enter key to continue.");
+        sc.nextLine();
     }
 }
